a83a72f3f95685690540ca2c227b329c1f576100
soma322 <vampari58@gmail.com>
2023-11-12 23:00:55 -0700

    commit inicial

---
+++ src/components/LogoutModal.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { onMount } from 'svelte';
+  
+    onMount(() => {
+      // Add any initialization logic here
+    });
+  </script>
+  
+  <!-- Your HTML and UI code for the logout modal -->
+  <div>
+    <!-- Add your modal content here -->
+    <div class="modal fade" tabindex="-1" role="dialog">
+      <!-- ... -->
+    </div>
+  </div>
+  
+  <style>
+    /* Your styles go here */
+  </style>
+  
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<head>
+    <!-- Other head elements -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+</head>
+
+<nav>
+    <a href="/"><i class="fas fa-home"></i></a>
+    <a href="/about"><i class="fas fa-info-circle"></i></a>
+    <a href="/account"><i class="fas fa-user"></i></a>
+    <a href="/applicacion"><i class="fas fa-mobile-alt"></i></a>
+</nav>
+
+
+
+<slot />
